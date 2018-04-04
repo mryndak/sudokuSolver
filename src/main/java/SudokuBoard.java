09e3cadb57f4931c7fcbaddd90d07ccd5c1c2755
Jaroslaw Soja <jarek.soja@gmail.com>
2018-04-04 17:01:29 +0200

    Reworked the board view

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -22,9 +22,15 @@ public class SudokuBoard {
         for (int i = 0; i < 9; i++) {
             List<Integer> printedList = sudokuBoard.get(i);
             for (int j = 0; j < 9; j++) {
-                System.out.print(printedList.get(j));
+                System.out.print("[" + printedList.get(j) + "]");
+                if (j == 2 || j == 5) {
+                    System.out.print("*");
+                }
             }
             System.out.println();
+            if (i == 2 || i == 5) {
+                System.out.println("*****************************");
+            }
         }
     }
 }
